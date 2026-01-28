adec30c59e437d7d0aeab92347db4729cfc9fcb5
Alvaro Montoro <alvaromontoro@gmail.com>
2026-01-27 23:42:55 -0600

    add fix for silly android issue

--- games/css/chains.js
+++ games/css/chains.js
@@ -24,36 +24,58 @@ document.querySelector(".check").addEventListener("click", () => {
   }
 })
 
-// allow better keyboard experience
-document.querySelectorAll(".chain input").forEach(input => {
-  input.addEventListener("keydown", e => {
 
-    // tab or space: nothing (moves or deletes)
-    if (["Tab","Space"].indexOf(e.code) >= 0) {
-      return;
-    }
-    
-    // delete/backspace: delete cell and move to previous
-    if (e.code === "Backspace") {
-      input.value = "";
-      input.previousElementSibling?.focus();
-    }
-    
-    // letter key: add key and move to next input
-    if (e.code >= "KeyA" && e.code <= "KeyZ") {
-      input.value = e.code[3];
-      input.nextElementSibling?.focus();
-    }
-    
-    // add navigation with arrows
-    if (e.code === "ArrowLeft") {
-      input.previousElementSibling?.focus();
-    }
-    if (e.code === "ArrowRight") {
-      input.nextElementSibling?.focus();
-    }
-    
-    // prevent typing again
-    e.preventDefault();
+window.mobileCheck = function() {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i
+  ];
+
+  return toMatch.some((toMatchItem) => {
+    return navigator.userAgent.match(toMatchItem);
+  });
+};
+
+// disabling the JS logic on mobile because Android's virtual keyboard doesn't play nice with keycodes and keyEvents
+if (!mobileCheck()) {
+  // allow better keyboard experience
+  document.querySelectorAll(".chain input").forEach(input => {
+    input.addEventListener("keydown", e => {
+
+      // tab or space: nothing (moves or deletes)
+      if (["Tab","Space"].indexOf(e.code) >= 0) {
+        return;
+      }
+      
+      // delete/backspace: delete cell and move to previous
+      if (e.code === "Backspace") {
+        input.value = "";
+        input.previousElementSibling?.focus();
+      }
+      
+      // letter key: add key and move to next input
+      if (e.code >= "KeyA" && e.code <= "KeyZ") {
+        input.value = e.code[3];
+        input.nextElementSibling?.focus();
+      }
+      
+      // add navigation with arrows
+      if (e.code === "ArrowLeft") {
+        input.previousElementSibling?.focus();
+      }
+      if (e.code === "ArrowRight") {
+        input.nextElementSibling?.focus();
+      }
+      
+      // prevent typing again
+      e.preventDefault();
+    })
   })
-})
+
+
+}
